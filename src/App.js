59dbffe1a9b8b49a867401e08220a471ce80cf6e
ushzkakoj <146225027+ushzkakoj@users.noreply.github.com>
2025-09-20 16:06:35 +0200

    update

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import dota from "./dota.jpg";
+
+function App() {
+  const name = "Андрій";
+  const website = { name: "Dota", url: "https://www.dota2.com/home" };
+  const num1 = 6;
+  const num2 = 10;
+  const colors = ["Червоний", "Синій", "Зелений"];
+
+  return (
+    <div className="app">
+      <h1 className="app__title">{name}</h1>
+      <p className="app__welcome">Ласкаво просимо до нашого сайту!</p>
+      <img className="app__image" src={dota} alt="Dota logo" />
+      <a className="app__link" href={website.url}>
+        {website.name}
+      </a>
+      <p className="app__sum">Сума: {num1 + num2}</p>
+      <ul className="list">
+        {colors.map((color) => (
+          <li key={color} className="list__item" data-color={color.toLowerCase()}>
+            {color}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
